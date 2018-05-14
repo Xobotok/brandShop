0b15384f915dcc9898e562ec7779b2e6b086a795
Xobotok <meshcheryakov.m.i@yandex.ru>
2018-05-14 14:12:42 +0500

    vsgd

--- app/index.php
+++ app/index.php
@@ -12,7 +12,7 @@
 </head>
 <body>
 <section class="header container">
-<a class="logo" href="index.html">
+<a class="logo" href="index.php">
     <img src="img/index/logo.png" alt="Logo" class="header__logo_image">
     <h3 class="animated__char header__logo_text">b</h3>
     <h3 class="header__logo_text">ran <span>D</span></h3>
